d3999d001e93b82af29e8f457ceace12895696b6
Nicole <nicoleaugusti@yahoo.it>
2024-07-02 19:16:56 +0200

    Prima traccia del primo esercizio PHP

---
+++ 1eserciziophp.php
@@ -0,0 +1,13 @@
+<?php
+
+//Traccia1: Definisci 4 variabili: integer, float, string , boolean. A schermo, fai comparire il tipo di dato delle varie variabili.
+
+$numero_intero= 5;
+$numero_decimale= 4.5;
+$stringa= "Ciao, sono Nicole";
+$booleano= true;
+
+var_dump($numero_decimale);
+var_dump($numero_intero);
+var_dump($stringa);
+var_dump($booleano);
